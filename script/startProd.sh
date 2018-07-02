a5951c9f04dc49d887932470a43226c5423c6702
SabertoothX6 <sabertoothx6@hotmail.de>
2018-07-02 14:28:09 +0200

    understanding probs

--- script/startProd.sh
+++ script/startProd.sh
@@ -4,7 +4,8 @@ docker pull asset.allgaeu-parcel-service.de:5000/parcelservice-frontend
 docker pull asset.allgaeu-parcel-service.de:5000/parcelservice-proxy
 
 docker run -d --restart always --network=ParcelService --name=db asset.allgaeu-parcel-service.de:5000/parcelservice-database
-docker run -d --restart always --network=ParcelService --name=restgreen asset.allgaeu-parcel-service.de:5000/parcelservice-server
+docker run -d --restart always --network=ParcelService --name=restgreen1 asset.allgaeu-parcel-service.de:5000/parcelservice-server
+docker run -d --restart always --network=ParcelService --name=restgreen2 asset.allgaeu-parcel-service.de:5000/parcelservice-server
 docker run -d -p 80:80 --restart always --network=ParcelService --name=webserver asset.allgaeu-parcel-service.de:5000/parcelservice-frontend
 docker run -d -p 8443:8443 --restart always --network=ParcelService --name=proxy asset.allgaeu-parcel-service.de:5000/parcelservice-proxy
 
